bf6e761961b366e453575b7dab50f5eaced058a1
nbayazes <nicholas.bayazes@gmail.com>
2026-07-20 12:22:01 -0400

    Add directory watcher

---
+++ src/neon/DirectoryWatcher.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+#include <filesystem>
+#include <thread>
+#include <unordered_set>
+
+namespace neon {
+
+class DirectoryWatcher {
+    std::filesystem::path _directory;
+    std::thread _worker;
+    std::atomic<bool> _alive;
+    HANDLE _handle = nullptr;
+
+public:
+    DirectoryWatcher(std::filesystem::path directory)
+        : _directory(std::move(directory)) {
+        _worker = std::thread(&DirectoryWatcher::Worker, this);
+    }
+
+    DirectoryWatcher(const DirectoryWatcher& other) = delete;
+    DirectoryWatcher(DirectoryWatcher&& other) noexcept = delete;
+    DirectoryWatcher& operator=(const DirectoryWatcher& other) = delete;
+    DirectoryWatcher& operator=(DirectoryWatcher&& other) noexcept = delete;
+
+    ~DirectoryWatcher() {
+        _alive = false;
+
+        if (_worker.joinable())
+            _worker.join();
+    }
+
+private:
+    void Worker();
+};
+
+class DirectoryChangeNotifier {
+    HANDLE _watcherHandle{};
+    OVERLAPPED _overlapped{};
+    std::vector<std::byte> _buffer;
+    std::unordered_set<std::filesystem::path> _filesToWatch;
+
+public:
+    DirectoryChangeNotifier(const std::filesystem::path& path, size_t bufferSize);
+
+    DirectoryChangeNotifier(const DirectoryChangeNotifier& other) = delete;
+    DirectoryChangeNotifier(DirectoryChangeNotifier&& other) noexcept = delete;
+    DirectoryChangeNotifier& operator=(const DirectoryChangeNotifier& other) = delete;
+    DirectoryChangeNotifier& operator=(DirectoryChangeNotifier&& other) noexcept = delete;
+
+    ~DirectoryChangeNotifier();
+
+    void AddFileToWatch(const std::filesystem::path& path) {
+        _filesToWatch.insert(path);
+    }
+
+    void StopWatchingFiles() {
+        _filesToWatch.clear();
+    }
+
+    std::unordered_set<std::filesystem::path> GetChangedFiles();
+};
+
+}
---
+++ src/neon/WorkerThread.h
@@ -0,0 +1,70 @@
+#pragma once
+#pragma once
+
+#include <thread>
+#include <mutex>
+#include <spdlog/spdlog.h>
+
+// A long running worker thread that waits for notifications to start processing
+class WorkerThread {
+    std::mutex _notifyLock;
+    std::condition_variable _workAvailable;
+    std::thread _worker;
+    std::atomic<bool> _hasWork;
+    std::atomic<bool> _alive;
+    std::string _name;
+
+public:
+    WorkerThread(std::string_view name) : _name(name) {}
+    virtual ~WorkerThread() { Stop(); }
+
+    void Start() {
+        assert(!_alive);
+        _alive = true;
+        _worker = std::thread(&WorkerThread::Worker, this);
+    }
+
+    void Stop() {
+        if (!_alive) return;
+        _alive = false;
+        _workAvailable.notify_all();
+        if (_worker.joinable())
+            _worker.join();
+    }
+
+    WorkerThread(const WorkerThread&) = delete;
+    WorkerThread(WorkerThread&&) = delete;
+    WorkerThread& operator=(const WorkerThread&) = delete;
+    WorkerThread& operator=(WorkerThread&&) = delete;
+
+    // Wake up the worker
+    void Notify() {
+        _hasWork = true;
+        _workAvailable.notify_one();
+    }
+
+protected:
+    virtual void Work() = 0;
+    bool HasWork() { return _hasWork; }
+
+private:
+    void Worker() {
+        SPDLOG_INFO("Starting worker `{}`", _name);
+        while (_alive) {
+            try {
+                _hasWork = false;
+                Work();
+
+                // New work could be requested while work is being done, so check before sleeping
+                if (!_hasWork) {
+                    std::unique_lock lock(_notifyLock);
+                    _workAvailable.wait(lock); // sleep until work requested
+                }
+            }
+            catch (const std::exception& e) {
+                SPDLOG_ERROR(e.what());
+            }
+        }
+        SPDLOG_INFO("Stopping worker `{}`", _name);
+    }
+};
